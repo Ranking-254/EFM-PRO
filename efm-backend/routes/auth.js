25cba2f693d445956f478078a6c1b0e5dbef2a96
Ranking-254 <pattinnjue@gmail.com>
2026-06-01 21:57:22 +0300

    fixed case sensitive for render

--- efm-backend/routes/auth.js
+++ efm-backend/routes/auth.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs'); // Note: Consider implementing password hashing in register/login later!
 const jwt = require('jsonwebtoken');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 
 // @desc    Manager login (by username or eFootball ID)
 // @route   POST /api/v1/auth/login
--- efm-backend/routes/league.js
+++ efm-backend/routes/league.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const League = require('../models/League');
-const User = require('../models/User');
+const User = require('../models/user');
 const Fixture = require('../models/Fixture'); 
 const generateRoundRobin = require('../utils/scheduler'); 
 
